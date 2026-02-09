6b6e6a8ebb0968d74bd87b7e06d2e37d44cf7c2b
NC4L4NGL4NG <calanglang.nea@obmontessori.edu.ph>
2026-02-09 14:12:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from pyscript import display, document
 def handle_signup(event):
     user_input = document.querySelector("#username")
     pass_input = document.querySelector("#password")
-    message_div = document.querySelector("#message")
+    message = document.querySelector("#message")
 
     if not user_input.value or not pass_input.value:
-        message_div.innerText = "Please fill in all fields."
+        message.innerText = "Please fill in all fields."
     
     elif len(pass_input.value) < 3:
-        message_div.innerText = "Password is too short."
+        message.innerText = "Password is too short."
     
     else:
-        message_div.innerText = "Account created. You may now log in using your credentials."
+
+        message.innerText = "Account created. You may now log in using your credentials."
